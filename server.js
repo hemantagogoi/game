5d90670b5915143127c07d2b1ad234927fae10c4
unknown <Hemanta Gogoi>
2026-04-05 15:25:52 +0530

    Fix Render deployment - update server routing and CSS path

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from current directory
 app.use(express.static(__dirname));
 
-// Serve index.html for root route
+// Serve tiktak.html for root route
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'tiktak.html'));
+});
+
+// Catch all handler - send back the index.html
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'tiktak.html'));
 });
 
